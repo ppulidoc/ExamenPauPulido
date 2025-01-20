46e5c3614a26534a6bb4b14300a52305bbe382ea
paupc <p.pulido.c@institutvidreres.cat>
2025-01-20 11:24:45 +0100

    bottomMenu i fragment contacta

--- app/src/main/java/com/paudam/examenpaupulido/MainActivity.kt
+++ app/src/main/java/com/paudam/examenpaupulido/MainActivity.kt
@@ -6,14 +6,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.paudam.examenpaupulido.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding:ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         enableEdgeToEdge()
 
-
-    }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupWithNavController(binding.bottomNavigationView, navController)
+}
 }
